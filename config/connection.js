83c2d3ef8c77875959c0288c80a67ef6a4ed18ca
Nelson Ezeume <nelsonezeume@yahoo.com>
2020-03-17 20:42:47 -0400

    added updated jawsdb connection

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,23 @@
 var mysql = require("mysql");
 require("dotenv").config();
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Kenechukwu22",
-  database: "burgers_db"
-});
+var connection;
+//if we have jawsdb, use that as connection
+if (process.env.JAWSDB_URL){
+  connection=mysql.createConnection(process.env.JAWSDB_URL);
+}
+else{
+  //else connect locallu
+  connection= mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Kenechukwu22",
+    database: "burgers_db"
+  });
+  
+
+}
 
 // Make connection.
 connection.connect(function(err) {
